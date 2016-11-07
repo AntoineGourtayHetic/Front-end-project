948df04d412a7926c10a85bde19755e77bd2e47d
« <cyprien.castel@hetic.net>
2016-11-07 16:50:38 +0100

    [main.js] clean code and add comments to explain

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,67 +11,72 @@ var arrows         = document.querySelectorAll('.arrow'),
         clientY: 0
     };
 
+//DETECT WHEN THE USER RESIZES HIS WINDOW TO UPDATE THE VARIABLES
 window.addEventListener('resize', updateUserViewPort);
+// DETECT WHEN THE USER PRESSES A KEY TO MOVE THE VIEWS
 window.addEventListener('keyup', keyboardNav);
+// DETECT WHEN THE USER STARTS TOUCHING HIS SCREEN
 window.addEventListener('touchstart', function(e) {
     userTouchStartCoord.clientX = e.touches[0].clientX;
     userTouchStartCoord.clientY = e.touches[0].clientY;
 });
-
+// DETECT WHEN THE USER MOVES HIS FINGER ON HIS SCREEN
 window.addEventListener('touchmove', function(e) {
 
     userTouchMoveCoord.clientX = e.touches[0].clientX;
     userTouchMoveCoord.clientY = e.touches[0].clientY;
 });
-
+// DETECT WHEN THE USER DOESN'T TOUCH HIS SCREEN ANYMORE
 window.addEventListener('touchend', touchNav);
 
-// SET LISTENERS
+// SET LISTENERS ON THE ARROWS
 for(var i = 0; i < arrows.length; i++) {
     arrows[i].addEventListener('click', moveView);
 }
 
+
+// FUNCTION TO MOVE THE VIEWS CONTAINER
 function moveView(e) {
 
-    // GET THE CLASS TO KNOW WHICH ARROW IS CLICKED
     var target;
-
+    // IF THE USER USES THE HTML ARROWS
     if(typeof e === 'object') {
-
+        // GET THE LAST OF ITS CLASS TO GUESS WHICH ARROW HE CLICKED
         target = e.target.classList[e.target.classList.length - 1];
     } else {
+        // IF HE DOESN'T USE THE ARROWS WE ALREADY KNOW THANKS TO THE PARAM
         target = e;
     }
 
-        //GET THE CSS OF THE ARROW
-        var viewsContainerComputedStyle = window.getComputedStyle(viewsContainer),
+    // GET THE CSS VALUES OF THE ARROW
+    var viewsContainerComputedStyle = window.getComputedStyle(viewsContainer),
 
-        //GET THE TRANSLATE VALUE VIA FUNCTION getTranslateValue
-            translateValues = getTranslateValue(viewsContainerComputedStyle.getPropertyValue('transform'));
+        // GET THE TRANSLATE VALUE VIA FUNCTION getTranslateValue (IT RETURNS A MATRIX())
+        translateValues = getTranslateValue(viewsContainerComputedStyle.getPropertyValue('transform'));
 
     switch(target) {
 
         case 'navArrow__topArrow':
             viewsContainer.style.transform = 'matrix(1, 0, 0, 1, ' + parseInt(translateValues[4] ) + ', ' + (parseInt(translateValues[5]) + userViewHeight) + ')';
-        break;
+            break;
 
         case 'navArrow__rightArrow':
             viewsContainer.style.transform = 'matrix(1, 0, 0, 1, ' + (parseInt(translateValues[4]) - userViewWidth) + ', ' + parseInt(translateValues[5]) + ')';
-        break;
+            break;
 
         case 'navArrow__bottomArrow':
             viewsContainer.style.transform = 'matrix(1, 0, 0, 1, ' + parseInt(translateValues[4]) + ', ' + parseInt(translateValues[5] - userViewHeight) + ')';
-        break;
+            break;
 
         case 'navArrow__leftArrow':
             viewsContainer.style.transform = 'matrix(1, 0, 0, 1, ' + (parseInt(translateValues[4]) + userViewWidth) + ', ' + parseInt(translateValues[5]) + ')';
-        break;
+            break;
 
     }
 
-    console.log(translateValues);
 }
 
+// RETURN AN ARRAY OF THE TRANSLATE VALUES
 function getTranslateValue(matrix) {
     var values = matrix.split('(')[1],
         values = values.split(')')[0],
@@ -80,6 +85,8 @@ function getTranslateValue(matrix) {
     return values;
 }
 
+
+// UPDATE THE VIEWPORT VALUE WHEN THE USER RESIZES HIS WINDOW
 function updateUserViewPort(arg) {
     if(arg == 'width') {
         return window.innerWidth;
@@ -89,12 +96,9 @@ function updateUserViewPort(arg) {
 
 }
 
+// FUNCTION TO HANDLE TOUCH NAV DEPENDING ON THE GESTURE
 function touchNav(e) {
 
-    console.log('user start y ' + userTouchStartCoord.clientY);
-    console.log('user end y ' + userTouchMoveCoord.clientY);
-    console.log('user start x ' + userTouchStartCoord.clientX);
-    console.log('user end x ' + userTouchMoveCoord.clientX);
 
     var redirect = redirectTouchNav();
 
@@ -102,44 +106,49 @@ function touchNav(e) {
 
         case 'RIGHT TO LEFT':
             moveView('navArrow__rightArrow');
-        break;
+            break;
         case 'LEFT TO RIGHT':
             moveView('navArrow__leftArrow');
-        break;
+            break;
         case 'BOTTOM TO TOP':
             moveView('navArrow__bottomArrow');
-        break;
+            break;
         case 'TOP TO BOTTOM':
             moveView('navArrow__topArrow');
-        break;
+            break;
 
     }
 
-    // FROM RIGHT TO LEFT IF((userTouchStartCoord.clientX - userTouchMoveCoord.clientX) > (userTouchStartCoord.clientY - userTouchMoveCoord.clientX))
-
 }
 
+// FUNCTION TO INTERPRET THE GESTURE
 function redirectTouchNav() {
-    // TOUCH FROM TOP TO BOTTOM
-    // TOUCH FROM BOTTOM TO TOP
-    // TOUCH FROM LEFT TO RIGHT
-    // TOUCH FROM RIGHT TO LEFT
+    /*
+     * COMPARE THE GAP BETWEEN THE TOUCH START COORDS AND THE TOUCH END COORDS
+     * AND THE DIFF BETWEEN THE SOUSTRACTION OF THE TOUCH START AND TOUCH END X AND THE TOUCH START Y AND TOUCH START Y
+     * */
     if((userTouchMoveCoord.clientY > userTouchStartCoord.clientY) && ((userTouchMoveCoord.clientY - userTouchStartCoord.clientY) > (userTouchMoveCoord.clientX - userTouchStartCoord.clientX))) {
+
         return 'TOP TO BOTTOM';
+
     } else if((userTouchMoveCoord.clientY < userTouchStartCoord.clientY) && ((userTouchMoveCoord.clientY - userTouchStartCoord.clientY) < (userTouchMoveCoord.clientX - userTouchStartCoord.clientX))) {
 
         return  'BOTTOM TO TOP';
+
     } else if((userTouchMoveCoord.clientX > userTouchStartCoord.clientX) && ((userTouchMoveCoord.clientY - userTouchStartCoord.clientY) < (userTouchMoveCoord.clientX - userTouchStartCoord.clientX))) {
 
         return 'LEFT TO RIGHT';
+
     } else if((userTouchMoveCoord.clientX < userTouchStartCoord.clientX) && ((userTouchMoveCoord.clientY - userTouchStartCoord.clientY) > (userTouchMoveCoord.clientX - userTouchStartCoord.clientX))) {
 
         return 'RIGHT TO LEFT';
+
     }
 }
 
+// FUNCTION TO HANDLE KEYBOARD NAV
 function keyboardNav(e) {
-
+    // GET KEYCODE OF PRESSED KEY
     var key = e.keyCode;
 
     switch(key) {
@@ -149,28 +158,28 @@ function keyboardNav(e) {
         case 38:
         case 90:
             moveView('navArrow__topArrow');
-        break;
+            break;
 
         // LEFT AND Q
 
         case 37:
         case 81:
             moveView('navArrow__leftArrow');
-        break;
+            break;
 
         // DOWN AND S
 
         case 40:
         case 83:
             moveView('navArrow__bottomArrow');
-        break;
+            break;
 
         // RIGHT AND D
 
         case 39:
         case 68:
             moveView('navArrow__rightArrow');
-        break;
+            break;
 
     }
 }
