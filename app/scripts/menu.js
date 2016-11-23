93179c50476f3ec151ebb2c2803548e28037d5ef
m-a-l <m-a-les@hotmail.fr>
2016-11-23 21:33:40 +0100

    [FIX] fixed missing menus deleted by git, added view firewatch1

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -1,8 +1,12 @@
 let menuIcon = document.querySelector('.menu__burger'),
-    menu     = document.querySelector('.menu')
+    menu     = document.querySelector('.menu'),
+    links    = menu.querySelectorAll('.menu__link')
     ;
 
 menuIcon.addEventListener('click', menuAppear);
+links.forEach(function(element) {
+    element.addEventListener('click', menuDisappear);
+}, this);
 function menuAppear() {
      if(menu.classList.length == 1) {
          menu.classList.add('open');
@@ -14,4 +18,13 @@ function menuAppear() {
      } else {
          menuIcon.classList.remove('open');
      }
+}
+
+function menuDisappear() {
+    if(menu.classList.length == 2) {
+         menu.classList.remove('open');
+     } 
+     if(menuIcon.classList.length == 2) {
+         menuIcon.classList.remove('open');
+     } 
 }
