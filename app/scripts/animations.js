83ae01028c6beef571c805cdce1041110e9700a1
« <cyprien.castel@hetic.net>
2016-11-23 13:05:41 +0100

    progressive river filling

--- app/scripts/animations.js
+++ app/scripts/animations.js
@@ -1,15 +1,15 @@
 let observer = new IntersectionObserver( entries => {
     console.log(entries);
     entries.forEach(entry => {
-        entry.target.classList.toggle('hideImageCircle');
-        entry.target.classList.toggle('showImageCircle');
+        entry.target.classList.toggle('hideImage');
+        entry.target.classList.toggle('showImage');
     });
 
 }, {
     threshold: [0.5]
 });
 
-const elems = document.querySelectorAll('.criterionImageCircle');
+const elems = document.querySelectorAll('.hideImage');
 
 elems.forEach((item) => {
     observer.observe(item);
